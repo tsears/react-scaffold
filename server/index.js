ff11faf0eea0e98d004633ccb60bc9ac6025c725
Tom Sears <tom@tsears.net>
2019-03-03 16:15:24 -0800

    Dockerize

--- server/index.js
+++ server/index.js
@@ -4,5 +4,9 @@ const port = process.env.PORT || 8081
 const app = express()
 
 app.get('/ayy', (req, res) => res.json({ response: 'lmao' }))
+app.get('*', (req, res) => {
+  console.log('REQUEST FROM: %s FOR RESOURCE: %s', req.hostname, req.originalUrl)
+  res.json({})
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
